8eabe6ef9b95a78a92a05640765744ad131dc0c5
oleksii.balanenko <oleksii.balanenko@justanswer.com>
2024-10-15 22:28:26 +0300

    Fix

--- src/app/bot-handlers/getSheetHandler.ts
+++ src/app/bot-handlers/getSheetHandler.ts
@@ -2,6 +2,8 @@ import { Context } from 'telegraf';
 import { GoogleSheetPageUrlTemplate } from '../constants';
 
 export const getSheetHandler = async (ctx: Context) => {
-    const url = GoogleSheetPageUrlTemplate.replace('{sheetId}', process.env.SPREADSHEET_ID || '');
+    const url = GoogleSheetPageUrlTemplate
+                .replace('{spreadsheetId}', process.env.SPREADSHEET_ID || '')
+                .replace('{sheetId}', '0');
     await ctx.reply(`Ось посилання на файл: [Відкрити](${url})`, { parse_mode: 'MarkdownV2',  link_preview_options: { is_disabled: true } });
 };
