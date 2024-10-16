a439dcc2b33b6aa7b71ec7b4431b545982c5b11e
oleksii.balanenko <oleksii.balanenko@justanswer.com>
2024-10-16 12:14:57 +0300

    On more

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -4,5 +4,5 @@ export const Scopes = ['https://www.googleapis.com/auth/spreadsheets'];
 export const ApplicationName = 'ExpenseManager';
 export const DefaultColumnCount = 5;
 export const GoogleSheetDateTimeFormat = 'MM/dd/yyyy HH:mm:ss';
-export const GoogleSheetPageUrlTemplate = 'https://docs.google.com/spreadsheets/d/{spreadsheetId}/edit?usp=sharing';
+export const GoogleSheetPageUrlTemplate = 'https://docs.google.com/spreadsheets/d/{spreadsheetId}/edit?usp=sharing&gid={sheetId}#gid={sheetId}';
 
